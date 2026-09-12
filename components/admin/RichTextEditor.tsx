4a2aaa77d5a73e9210cc6a30bf480fb72a92e76c
GianbaCrown <269571140+GianbaCrown@users.noreply.github.com>
2026-09-12 16:07:19 +0200

    richeditor

--- components/admin/RichTextEditor.tsx
+++ components/admin/RichTextEditor.tsx
@@ -21,7 +21,7 @@ export default function RichTextEditor({
 
   useEffect(() => {
     if (editor && value !== editor.getHTML()) {
-      editor.commands.setContent(value || '', false)
+            editor.commands.setContent(value || '', { emitUpdate: false })
     }
   }, [value, editor])
 
